73af61d80d08e1e940419fdc57b330d3f4285c7f
mJamandron21 <mjamandron@gmail.com>
2022-05-03 22:13:25 +0800

    Adding Login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from "styled-components";
+
+export default function Login() {
+  return <Container>login</Container>; 
+}
+
+const Container =  styled.div`
+
+`; 
