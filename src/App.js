61985f3bf92ce9bc782d14559f770baece0cc6ad
mJamandron21 <mjamandron@gmail.com>
2022-05-06 03:43:39 +0800

    Adding reducerCases to check if token is present

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, {useEffect} from 'react'
-import Login from './components/Login'
+import React, { useEffect } from 'react';
+import Login from './components/Login';
+import Spotify from './components/Spotify';
+import { reducerCases } from './utils/Constants';
+import { useStateProvider } from "./utils/StateProvider";
 
 export default function App() {
+  const [{ token }, dispatch] = useStateProvider();
+
   useEffect(()=>{
     const hash = window.location.hash;
 
     if(hash){
       const token = hash.substring(1).split("&")[0].split("=")[1];
-      console.log(token)
+      // console.log(token)
+      dispatch({ type:reducerCases.SET_TOKEN,token });
     }
-  },[])
+  },[token, dispatch]);
+
   return (
     <div>
-      <Login />
+      {token ? <Spotify /> : <Login />}
     </div>
   )
 }
