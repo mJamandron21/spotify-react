6b8bc9d77d698bf82514c93cde1cf48029e00bdd
mJamandron21 <mjamandron@gmail.com>
2022-05-06 03:14:39 +0800

    Setup spotify access token

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import styled from "styled-components";
 
 export default function Login() {
+  const handleClick = () =>{
+    const clientId = "203156920f4340459304f5935f881757";
+    const redirectUrl = "http://localhost:3000/";
+    const apiUrl = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-email",
+      "user-read-private",
+      "user-modify-playback-state",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "user-read-recently-played",
+      "user-read-playback-position",
+      "user-top-read"
+    ];
+
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")}&response_type=token&show_dialog=true`;
+  };
   return <Container>
       <img src = "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt = "spotify-logo"/>
-      <button>Connect Spotify</button>
+      <button onClick={handleClick}>Connect Spotify</button>
   </Container>; 
 }
 
