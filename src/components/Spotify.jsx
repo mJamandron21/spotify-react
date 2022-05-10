b1cfaa6661df4a6f804e13c397f1da9693dda069
mJamandron21 <mjamandron@gmail.com>
2022-05-11 00:59:21 +0800

    Adding design using styled-components

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -25,5 +25,22 @@ export default function Spotify() {
 }
 
 const Container = styled.div`
-
+  max-width: 100vw;
+  max-height: 100vh;
+  overflow: hidden;
+  display: grid;
+  grid-template-rows: 85vh 15vh;
+  .spotify-body {
+    display: grid;
+    grid-template-columns: 15vw 85vw;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(transparent, rgba(0,0,0,1));
+    background-color: rgb(32,87,100);
+    .body {
+      height: 100%;
+      width:  100%;
+      overflow: auto;
+    }
+  }
 `;
