89427bb481da9ed49e0d01974c4025226f216a1a
mJamandron21 <mjamandron@gmail.com>
2022-05-15 02:19:12 +0800

    Adding design in to playlist.jsx using styled comp

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useStateProvider } from '../utils/StateProvider'
 import axios from "axios";
+import styled from 'styled-components';
 import { reducerCases } from '../utils/Constants';
 
 export default function Playlist() {
@@ -24,14 +25,47 @@ export default function Playlist() {
     }, [token, dispatch]);
 
   return (
-    <div>
+    <Container>
         <ul>
             {
                 playlists.map(({ name, id}) => {
                     return <li key={id}>{name}</li>;
                 })
+                
             }
+        
         </ul>
-    </div>
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+height: 100%;
+overflow: hidden;
+ul{
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 50vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+        width: 0.7rem;
+        &-thumb {
+            background-color: rgba(255,255,255,0.2);
+        }
+    }
+    li{
+      display: flex;
+      gap: 1rem;
+      cursor: pointer;
+      transition: 0.3s ease-in-out;
+      &:hover {
+          color: white;
+      }
+    }
+  }
+`;
