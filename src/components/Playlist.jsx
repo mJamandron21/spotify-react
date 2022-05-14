7ee69822e82b125868db7555b4b45af84822bd5b
mJamandron21 <mjamandron@gmail.com>
2022-05-15 00:03:49 +0800

    Fetch playlist from spotify api -axios

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -6,7 +6,18 @@ export default function Playlist() {
     const [{ token, dispatch }] = useStateProvider();
     useEffect(() => {
         const getPlaylistData = async () => {
-            const response = await axios.get();
+            const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+                headers: {
+                    Authorization: "Bearer " + token,
+                    "Content-Type": "application/json",
+
+                },
+            });
+            const { items } = response.data;
+            const playlists = items.map(({name,id})=> {
+                return {name,id};
+            }); 
+            console.log(playlists)
         };
         getPlaylistData();
     }, [token, dispatch]);
