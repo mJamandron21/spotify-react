bccdb26ea7c676d4b94f25dc55e7ddb25f5f3c1c
mJamandron21 <mjamandron@gmail.com>
2022-05-17 21:48:00 +0800

    Designing Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,5 +24,54 @@ export default function Navbar() {
 }
 
 const Container = styled.div`
-
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 1rem 2rem;
+height: 15vh;
+position: sticky;
+top: 0;
+transition: 0.3s ease-in-out;
+.search_bar {
+  background-color: white;
+  width: 30%;
+  padding: 0.2rem 0.7rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  border-radius: 2rem;
+  input {
+    border: none;
+    height: 2rem;
+    width: 100%;
+    &:focus {
+      outline: none;
+    }
+  }
+}
+.avatar {
+  background-color: black;
+  padding: 0.2rem 0.4rem;
+  padding-right: 1rem;
+  border-radius: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+    svg {
+      font-size: 1.3rem;
+      background-color: #282828;
+      padding: 0.2rem;
+      border-radius: 1rem;
+      color: #c7c5c5;
+    }
+  }
+}
 `;
