a47bc93bf5b73981cd70b402387f362cdc847942
mJamandron21 <mjamandron@gmail.com>
2022-05-19 02:58:35 +0800

    Successful selectedplaylist details displayed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -48,6 +48,72 @@ export default function Body() {
 
   return (
     <Container>
+      {selectedPlaylist && (
+        <>
+        <div className="playlist">
+          <div className="image">
+            <img src={selectedPlaylist.image} alt="selectedplaylist" />
+          </div>
+          <div className="details">
+            <span className='type'>PLAYLIST</span>
+            <h1 className='title'>{selectedPlaylist.name}</h1>
+            <p className='description'>{selectedPlaylist.description}</p>
+          </div>
+        </div>
+        <div className="list">
+          <div className="header_row">
+            <div className="col">
+              <span>#</span>
+            </div>
+            <div className="col">
+              <span>TITLE</span>
+            </div>
+            <div className="col">
+              <span>ALBUM</span>
+            </div>
+            <div className="col">
+              <span><AiFillClockCircle /></span>
+            </div>
+          </div>
+          <div className="tracks">
+            {selectedPlaylist.tracks.map(({
+              id,
+              name,
+              artists,
+              image,
+              duration,
+              album,
+              context_uri,
+              track_number,
+            }, index) => {
+              return (
+                <div className='row' key={id}>
+                  <div className="col">
+                    <span>{index+1}</span>
+                  </div>
+                  <div className="col detail">
+                    <div className="image">
+                      <img src={image} alt='track' />
+                    </div>
+                    <div className="info">
+                      <span className='name'>{name}</span>
+                      <span>{artists}</span>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <span>{album}</span>
+                  </div>
+                  <div className="col">
+                    <span>{duration}</span>
+                  </div> 
+                </div>
+              )
+            })}
+
+          </div>
+        </div>
+        </>
+      )}
       
     </Container>
   )
