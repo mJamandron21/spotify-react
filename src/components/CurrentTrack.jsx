36298772a0c874ffd605ba3aed12f927b232a9b3
mJamandron21 <mjamandron@gmail.com>
2022-05-23 21:32:33 +0800

    Setup api call for currentTrack

--- src/components/CurrentTrack.jsx
+++ src/components/CurrentTrack.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useStateProvider } from '../utils/StateProvider';
+import axios from 'axios';
 
-export default function CurrentTrack() {
-  return (
-    <div>CurrentTrack</div>
+export default function CurrentTrack() {    
+const [{ token }, dispatch] = useStateProvider();
+useEffect(() => {
+    const getCurrentTrack = async () => {
+        const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: {
+                Authorization: "Bearer " + token,
+                "Content-Type": "application/json",
+
+            },
+        }
+        );
+        console.log(response);
+        // dispatch({ type: reducerCases.SET_PLAYLISTS, playlists});
+    }
+    getCurrentTrack();
+}, [token, dispatch]);  
+
+
+return (
+    <Container>CurrentTrack</Container>
   )
 }
+
+const Container = styled.div`
+    
+`;
