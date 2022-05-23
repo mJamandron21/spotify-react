044917b179fd830bccdf436555f4e35a9ae098f2
mJamandron21 <mjamandron@gmail.com>
2022-05-23 21:05:16 +0800

    Adding CurrentTrack.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import CurrentTrack from './CurrentTrack';
 
 export default function Footer() {
   return (
-    <Container>Footer</Container>
+    <Container>
+      <CurrentTrack />
+    </Container>
   )
 }
 
