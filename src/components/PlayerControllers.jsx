48dfdc5ab2f9a80469941f78695e4aef209210d5
mJamandron21 <mjamandron@gmail.com>
2022-05-24 21:29:01 +0800

    Additional setups icons

--- src/components/PlayerControllers.jsx
+++ src/components/PlayerControllers.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components';
+import { 
+    BsFillPlayCircleFill,
+    BsFillPauseCircleFill,
+    BsShuffle,
+} from 'react-icons/bs';
+import {
+    CgPlayTrackNext,
+    CgPlayTrackPrev,
+} from 'react-icons/cg';
+import { FiRepeat } from 'react-icons/fi';
 
 export default function PlayerControllers() {
   return (
-    <div>PlayerControllers</div>
+    <Container>PlayerControllers</Container>
   )
 }
+
+
+const Container = styled.div`
+`;
